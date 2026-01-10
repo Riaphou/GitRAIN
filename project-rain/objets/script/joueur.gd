a16d350e03bb2d0e977fbea47092c08b163aebd3
nejjahiishaq2008-sketch <nejjahiishaq2008@gmail.com>
2026-01-10 15:33:14 +0100

    rotation du joueur

--- project-rain/objets/script/joueur.gd
+++ project-rain/objets/script/joueur.gd
@@ -11,15 +11,24 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction = 0 
+	if Input.is_action_pressed("left"):
+		direction += -1.0
+	if Input.is_action_pressed("right"):
+		direction += 1.0
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
+	
+	if Input.is_action_pressed("left"):
+		scale.x = -10
+	elif Input.is_action_pressed("right"):
+		scale.x = 10
