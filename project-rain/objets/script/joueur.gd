d283e6d3db31bdbcbe427898c5557e4e46ef06eb
Hebert Raphaël <raphounilsou@gmail.com>
2026-01-10 15:56:17 +0100

    Réparation player

--- project-rain/objets/script/joueur.gd
+++ project-rain/objets/script/joueur.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -16,19 +17,21 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = 0 
+	var direction = 0.0
 	if Input.is_action_pressed("left"):
 		direction += -1.0
 	if Input.is_action_pressed("right"):
 		direction += 1.0
-	if direction:
+		
+	if direction != 0.0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
 	
-	if Input.is_action_pressed("left"):
-		scale.x = -10
-	elif Input.is_action_pressed("right"):
-		scale.x = 10
+	if Input.is_action_just_pressed("left"):
+		$AnimatedSprite2D.flip_h = true
+	elif Input.is_action_just_pressed("right"):
+		$AnimatedSprite2D.flip_h = false
+	
