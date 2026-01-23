26f34dfe6b057f4726fc5828a6ecf2885666f5b9
Hebert Raphaël <raphounilsou@gmail.com>
2026-01-23 16:43:47 +0100

    s

--- project-rain/codes/Globals.gd
+++ project-rain/codes/Globals.gd
@@ -5,3 +5,10 @@ var dialogInQueue = 0
 
 var positionPersonage = Vector2(0,0)
 var objetID = 0
+
+var condApparition = {
+	"start" = true,
+	"frigo" = false,
+	"casier1" = false,
+	"carier2" = false
+}
