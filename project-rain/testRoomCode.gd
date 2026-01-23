8c04425c3ff9c55196aabcf2ca470ebf484f4b19
nejjahiishaq2008-sketch <nejjahiishaq2008@gmail.com>
2026-01-23 16:35:30 +0100

    correctif sur la boite de dialogue
    corrige la boite de dialogue

--- project-rain/testRoomCode.gd
+++ project-rain/testRoomCode.gd
@@ -5,7 +5,6 @@ extends Node2D
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("click"):
 		Global.nbClics = Global.nbClics+1
-		if Global.nbClics > 4:
-			pass
+		if Global.nbClics == 4:
 			$"boîte de dialogue".queue_text("Bonjours les gens\nCool")
 			
