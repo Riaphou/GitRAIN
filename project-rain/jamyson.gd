6e1fc3ff5565653bae86c606a5f19c79f03ff5a2
RandomVart <tristanhg2010@gmail.com>
2026-01-23 11:42:39 +0100

    Déplacements
    Le perso se déplace jusqu'où on clique, et si on clique quand il se déplace, ça ne fait rien

--- project-rain/jamyson.gd
+++ project-rain/jamyson.gd
@@ -1,8 +1,24 @@
 extends CharacterBody2D
+var ax=0
+var ay=0
 
 func _ready():
 	pass
 	
-func _process(delta):
-	if Input.is_action_pressed("click_pressed"):
+func _physics_process(delta):
+	if ((ax-global_position.x)**2+(ay-global_position.y)**2)**0.5 <10: # Vérifie si le joueur est arrivé, la valeur à la fin est le nb de pixels de distance avec la cible oèu il s'arrête
+		ax = 0
+		ay = 0
+		velocity = Vector2.ZERO
+	if Input.is_action_just_pressed("click") and ax==0 and ay==0:
+		ax=get_global_mouse_position().x
+		ay=get_global_mouse_position().y
+		velocity.x = ax-global_position.x
+		velocity.y = ay-global_position.y
+		velocity = velocity.normalized() * 300 #Changez la valeur à la fin modifie la vitesse du joueur
+	move_and_slide()
+	
+		
+		
+		
 	
