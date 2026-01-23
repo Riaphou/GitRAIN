dd8ae005fcb4bd6ef29926b353df0ff1599f07c1
RandomVart <tristanhg2010@gmail.com>
2026-01-23 15:31:21 +0100

    small changes again

--- project-rain/jamyson.gd
+++ project-rain/jamyson.gd
@@ -12,10 +12,11 @@ func stop():
 func _ready():
 	pass
 	
-func _physics_process(delta):
+func _physics_process(_delta):
 	if ((ax-global_position.x)**2+(ay-global_position.y)**2)**0.5 <10: # Vérifie si le joueur est arrivé, la valeur à la fin est le nb de pixels de distance avec la cible oèu il s'arrête
 		stop()
 	if Input.is_action_just_pressed("click"):
+		Global.nbClics += 1
 		ax=get_global_mouse_position().x
 		ay=get_global_mouse_position().y
 		velocity.x = ax-global_position.x
