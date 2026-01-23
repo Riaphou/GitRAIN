187fb7df2fae50b4640752b0e7a11ac683d60d5b
RandomVart <tristanhg2010@gmail.com>
2026-01-23 21:57:27 +0100

    petits changements
    tentative infructueuse de mettre fondu au dessus de textbox

--- project-rain/fins/color_rect.gd
+++ project-rain/fins/color_rect.gd
@@ -1,14 +1,13 @@
 extends ColorRect
-  
-
-
+ 
 func _ready():
 	var col = color
 	col.a = 0
 	color = col
-	$dialogue.queue_text("no way") # une ligne pr le petit noam qui me parle d'opti h24
+	var listemots=["mot", "lettres","autremot"]	 #Ici sont les indices affichés à la fin du jeu, tu peux les modifier, Arthur
+	$dialogue.layer = 2 #Les layers ne marchent pas, à voir samedi ( textbox au dessus de fondu)
+	$dialogue.queue_text(listemots[randi() % listemots.size()]) 
 	await get_tree().create_timer(3).timeout
-	#Insére dialogues
 	for _i in range(100):
 		col.a += 0.01
 		color = col
