f5c18d215774904a815b5a01911f216d050f507a
Hebert Raphaël <raphounilsou@gmail.com>
2026-01-24 06:51:20 +0100

    passer dessus dessous

--- project-rain/map.gd
+++ project-rain/map.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
-
+var list = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	list.append($decor/Plante2X)
+	list.append($"decor/JusD'orangeX")
+	list.append($decor/Ordinateur3X)
+	list.append($decor/Ordinateur6X)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	for i in range(len(list)):
+		if list[i].global_position.y > Global.positionPersonage.y:
+			list[i].z_index = 1
+		else:
+			list[i].z_index = -1
